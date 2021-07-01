e5e1b1094521142c887ee4273c87f230ba313aab
Jonjon <jonjonclark@gmail.com>
2021-07-01 10:55:31 +0100

    Done first basic function of NFT auction. Added some package dependencies.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,5 +20,11 @@ task("accounts", "Prints the list of accounts", async () => {
  */
 module.exports = {
   solidity: "0.8.4",
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    gasPrice: 30,
+    coinmarketcap: "9aacee3e-7c04-4978-8f93-63198c0fbfef",
+  },
 };
 
