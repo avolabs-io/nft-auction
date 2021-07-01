6aeec83db1de85c0a4df25bd278abfebfc9c1b3d
MJ Young <mjyoung@MJs-MacBook-Air.local>
2021-07-01 14:29:20 +0200

    updated bid function to update highest bids and revert previous

--- contracts/NFTAuction.sol
+++ contracts/NFTAuction.sol
@@ -9,31 +9,55 @@ contract NFTAuction {
     mapping(uint256 => IERC721) public nftContracts;
     mapping(uint256 => uint256) public tokenIds;
     mapping(uint256 => uint256) public minPrices;
+    mapping(uint256 => uint256) public nftHighestBids;
+    mapping(uint256 => address) public nftHighestBidders;
 
     // constructor
     constructor() {
         console.log("Hello world");
     }
 
-    function newNftAuction(
+    function createNewNftAuction(
         address nftContractAddress,
         uint256 tokenId,
         uint256 minPrice
     ) public {
         // Sending our contract the NFT.
-        require(minPrice >0, "Minimum Price cannot be 0");
+        require(minPrice > 0, "Minimum Price cannot be 0");
         IERC721 nftContract = IERC721(nftContractAddress);
-        nftContract.safeTransferFrom(msg.sender, address(this), tokenId);
-
+        //Store the token id, token contract and minimum price
         nftIndex++;
         nftContracts[nftIndex] = nftContract;
         tokenIds[nftIndex] = tokenId;
         minPrices[nftIndex] = minPrice;
+        nftContract.safeTransferFrom(msg.sender, address(this), tokenId);
     }
 
     // bid functions.
-    function createBid(uint256 _nftIndex) public payable {
-        require(msg.value >= minPrices[_nftIndex]);
+    function makeBid(uint256 _nftIndex) public payable {
+        require(
+            msg.sender != nftContracts[_nftIndex].ownerOf(tokenIds[_nftIndex]),
+            "Owner cannot bid on own NFT"
+        );
+        require(
+            msg.value >= minPrices[_nftIndex],
+            "Bid must be higher than minimum bid"
+        );
+        require(
+            msg.value >= nftHighestBids[_nftIndex],
+            "Bid must exceed highest bid"
+        );
+
+        updateHighestBidAndRevertPreviousBid(_nftIndex);
+
         console.log(msg.value);
+    }
+
+    function revertPreviousBidAndUpdateHighestBid(uint256 _nftIndex) internal {
+        payable(nftHighestBidders[_nftIndex]).transfer(
+            nftHighestBids[_nftIndex]
+        );
+        nftHighestBids[_nftIndex] = msg.value;
+        nftHighestBidders[_nftIndex] = msg.sender;
     }
 }
