5a9b001257aacf225aa3783b12ae51e1918bc3f6
Jonjon <jonjonclark@gmail.com>
2021-07-08 17:37:03 +0100

    add optimizer

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,7 +19,15 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   gasReporter: {
     enabled: true,
     currency: "USD",
