fae4364d1daa1d215e8ed8124ccfe9aa1802e9c1
Mike Young <mj.young114@gmail.com>
2021-09-03 17:24:38 +0200

    Made changes to use hardhat deploy

---
+++ deploy/01_deployContracts.js
@@ -0,0 +1,21 @@
+const { network } = require("hardhat");
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  let nftAuction = await deploy("NFTAuction", {
+    from: deployer,
+    log: true,
+  });
+  let erc20 = await deploy("ERC20MockContract", {
+    from: deployer,
+    log: true,
+    args: ["Mock ERC20", "MER"],
+  });
+
+  let erc721 = await deploy("ERC721MockContract", {
+    from: deployer,
+    args: ["New Mocks", "NMK"],
+    log: true,
+  });
+};
