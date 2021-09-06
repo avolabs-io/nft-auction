762783558ef2b9a468c8fc58602bf7c857d5b18a
MJYoung114 <84841658+MJYoung114@users.noreply.github.com>
2021-09-06 19:53:09 +0200

    Update secretsManager.example.js

--- secretsManager.example.js
+++ secretsManager.example.js
@@ -1,7 +1,7 @@
 const rinkebyPrivateKey = "";
 const alchemyUrl = "";
 const etherscanApiKey = "";
-const coinmarketCapKey = "9aacee3e-7c04-4978-8f93-63198c0fbfef";
+const coinmarketCapKey = "";
 
 module.exports = {
   rinkebyPrivateKey,
